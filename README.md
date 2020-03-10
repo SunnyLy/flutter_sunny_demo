13eacb1046104c567fef5182e0969d0408417d0b
SunnyLy <450512747@qq.com>
2020-03-10 09:13:06 +0800

    整理Flutter学习线路，以及处理文本输入监听
    Signed-off-by: SunnyLy <450512747@qq.com>

--- README.md
+++ README.md
@@ -1,16 +1,54 @@
-# flutter_sunny_demo
+## Flutter学习线路
+根据Android开发经验，我将从下面这些知识点入手，一步步入门学习Flutter
+### 一、基础组件
+- Widget
+- Text(文本)、字体样式
+- TextField(文本编辑框)
+- MaterialButton(按钮)
+    - FlatButton
+    - RaisedButton
+- Image(图片)、Icon
+- 单选框、复选框
+- DateTime(时间组件)
 
-根据玩Android，初步学习Flutter的基本用法，先入门
 
-## Getting Started
+### 二、布局相关组件
+- Scaffold
+- Stack（层叠布局）
+- AppBar
+- Container(布局容器)
+- Expanded/Padding（布局填充）
+- Center
+- Column/Row(线性布局)
+- Flex(流式布局)
+- ClipOval()
+- SafeArea
+- SizeBox
 
-This project is a starting point for a Flutter application.
+### 滑动组件
+- SingleChildScrollView
+- ListView
+- GridView
+- PageView
+- 
 
-A few resources to get you started if this is your first Flutter project:
+### 三、页面路由
+- Navigator
+### Flutter动画
+- flutter动画原理
+- 动画种类及组件
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+### Flutter手势监听及事件传递
+### 网络请求
+### 数据库
+### 多线程
+### 自定义组件及 Pub仓库部署
+### Flutter第三方库
+    - RxDart
+    - BLoC
+### Flutter 配置文件介绍
+### Flutter自动化构建打包Apk
+###  Flutter 与 React Native对比
+### Flutter UI渲染机制
+### Flutter中Key作用
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sunny_demo/home/home.dart';
+import 'package:flutter_sunny_demo/utils/ToastUtil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  DateTime _lastClickTime; //上次点击的时间
   @override
   Widget build(BuildContext context) {
     var materialApp = MaterialApp(
@@ -16,8 +18,15 @@ class MyApp extends StatelessWidget {
       home: WillPopScope(
         child: MyHomePage(title: 'FlutterSunnyDemo'),
         onWillPop: () async {
-          Fluttertoast.showToast(msg: "点击了返回键");
-          return false;
+          if (_lastClickTime == null ||
+              (DateTime.now().difference(_lastClickTime) >
+                  Duration(seconds: 1))) {
+            _lastClickTime = DateTime.now();
+            Fluttertoast.showToast(msg: "再按一次返回桌面");
+            return false;
+          }
+
+          return true;
         },
       ),
     );
@@ -37,6 +46,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+  //用户名输入框
+  TextEditingController _userNameEditContr = TextEditingController();
+
+  //密码输入框
+  TextEditingController _pwdEditContr = TextEditingController();
 
   @override
   void initState() {
@@ -87,84 +102,103 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     print("xxxx:build()");
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-//            Image(image: AssetImage('images/demo_logo.png')),
-            ClipOval(
-              child: Image(
-                //用于加载图片
-                image: AssetImage('images/demo_logo.png'),
-                width: 100,
-                height: 100,
-                fit: BoxFit.fill,
-              ),
-            ), //圆形控件
-
-            //再添加一个layout,用于存放一个输入框，与一个文案提示
-            Container(
-              margin: EdgeInsets.all(15),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    '用户名:',
-                    style: TextStyle(
-                        color: Colors.black26,
-                        fontSize: 16,
-                        fontStyle: FontStyle.normal),
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: TextField(
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                            icon: Icon(Icons.person), hintText: "请输入用户名"),
-                        keyboardType: TextInputType.text, //会弹出文本输入键盘
-                      )), //用来限制输入框的宽高
-                ],
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.all(15),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "密码",
-                  ),
-                  Expanded(
-                      child: TextField(
-                    decoration: InputDecoration(
-                        icon: Icon(Icons.lock), hintText: "请输入密码"),
-                  ))
-                ],
-              ),
-            ),
-
-            Container(
-                width: 300,
-                height: 50,
-                margin: EdgeInsets.fromLTRB(15, 30, 15, 15),
-                child: FlatButton(
-                  onPressed: () {
-                    //登录成功，跳转至首次
-//                    Navigator.pushNamed(context, "/home/home.dart");
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return HomePage(title: "首页");
-                    }));
-                  },
-                  child: Text("登录"),
-                  color: Colors.blueAccent,
-                  textColor: Colors.white,
-                ))
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.fromLTRB(15, 50, 15, 0),
+          child: Center(
+            //body得用一个Scrollview来包裹，不然调出键盘时，会报Bottom overflowed by xxx pixels错误
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+//            Image(image: AssetImage('images/demo_logo.png')),
+                ClipOval(
+                  child: Image(
+                    //用于加载图片
+                    image: AssetImage('images/demo_logo.png'),
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fill,
+                  ),
+                ), //圆形控件
+
+                //再添加一个layout,用于存放一个输入框，与一个文案提示
+                Container(
+                  margin: EdgeInsets.all(15),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        '用户名:',
+                        style: TextStyle(
+                            color: Colors.black26,
+                            fontSize: 16,
+                            fontStyle: FontStyle.normal),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: TextField(
+                            maxLines: 1,
+                            controller: _userNameEditContr,
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.person), hintText: "请输入用户名"),
+                            keyboardType: TextInputType.text, //会弹出文本输入键盘
+                          )), //用来限制输入框的宽高
+                    ],
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(15),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "密码",
+                      ),
+                      Expanded(
+                          child: TextField(
+                        controller: _pwdEditContr,
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.lock), hintText: "请输入密码"),
+                      ))
+                    ],
+                  ),
+                ),
+
+                Container(
+                    width: 300,
+                    height: 50,
+                    margin: EdgeInsets.fromLTRB(15, 30, 15, 15),
+                    child: FlatButton(
+                      onPressed: () {
+                        //登录成功，跳转至首次
+//                    Navigator.pushNamed(context, "/home/home.dart");
+                        if (veryInputInfo()) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomePage(title: "首页");
+                          }));
+                        } else {
+                          ToastUtil.show("请输入用户名或密码");
+                        }
+                      },
+                      child: Text("登录"),
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                    ))
+              ],
+            ),
+          ),
+        ));
+  }
+
+  /// 验证输入信息
+  bool veryInputInfo() {
+    final username = _userNameEditContr.text;
+    final pwd = _pwdEditContr.text;
+    bool result = false;
+    result = (username.trim().isNotEmpty) && (pwd.trim().isNotEmpty);
+    return result;
   }
 }
