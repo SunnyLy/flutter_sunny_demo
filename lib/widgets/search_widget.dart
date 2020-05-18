1a9df77131d07cf8cbbb813b5dcd61ae39a4ca10
SunnyLy <450512747@qq.com>
2020-05-18 15:39:41 +0800

    添加清空icon。一键清空输入内容
    Signed-off-by: SunnyLy <450512747@qq.com>

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sunny_demo/utils/ToastUtil.dart';
 
 /// @Annotation <p>搜索控件</p>
 /// @Auth  Sunny
@@ -25,6 +26,17 @@ class SearchWidget extends StatefulWidget {
 }
 
 class _SearchWidgetState extends State<SearchWidget> {
+  static String _inputStr = "";
+
+  TextEditingController _textEditController =
+      TextEditingController.fromValue(TextEditingValue(text: _inputStr));
+
+  @override
+  void dispose() {
+    super.dispose();
+    _textEditController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,6 +47,7 @@ class _SearchWidgetState extends State<SearchWidget> {
       decoration: BoxDecoration(
           color: Colors.grey[50], borderRadius: BorderRadius.circular(15.0)),
       child: TextField(
+        controller: _textEditController,
         cursorColor: Colors.green[300],
         //光标颜色
         cursorWidth: 2,
@@ -46,14 +59,28 @@ class _SearchWidgetState extends State<SearchWidget> {
         maxLines: 1,
         decoration: InputDecoration(
           icon: widget.prefixIcon,
+          suffixIcon: GestureDetector(
+            child: Icon(
+              Icons.cancel,
+              color: Colors.grey[300],
+            ),
+            onTap: () {
+              //利用控制器来清空输入的内容
+              _textEditController.clear();
+            },
+          ),
+          //清除标志
           border: InputBorder.none,
           //去掉输入框下面的线
-          contentPadding: EdgeInsets.fromLTRB(2, 0, 0, 0),
+          contentPadding: EdgeInsets.fromLTRB(2, 10, 0, 0),
           //解决输入框没有垂直居中的问题
           hintMaxLines: 1,
           hintStyle: TextStyle(color: Colors.grey[300], fontSize: 24),
           hintText: widget.hint,
         ),
+        onChanged: (String inputStr) {
+          _inputStr = inputStr;
+        },
       ),
     );
   }
